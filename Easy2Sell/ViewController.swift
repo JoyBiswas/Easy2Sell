7d284238d97967636645d353ea03a58dae6c4567
JOY BISWAS <hello@joysmac.local>
2018-01-24 10:43:30 +0600

    Initial Commit

---
+++ Easy2Sell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy2Sell
+//
+//  Created by JOY BISWAS on 1/24/18.
+//  Copyright © 2018 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
